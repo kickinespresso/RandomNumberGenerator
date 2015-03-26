12fb380070f5fc22ae677c3647616556b2463bad
Chris <cdesch@gmail.com>
2015-03-26 11:07:14 -0400

    Initial Commit

---
+++ RandomNumberGenerator/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  RandomNumberGenerator
+//
+//  Created by cj on 3/26/15.
+//  Copyright (c) 2015 KickinEspresso. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
